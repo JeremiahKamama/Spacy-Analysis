bd1505791c535613ee312939ba821d96d7c3f795
Jeremiah Kamama <jeremiahkamama@gmail.com>
2021-05-21 19:31:39 +0300

    restructure

--- model.py
+++
@@ -1,8 +0,0 @@
-import spacy 
-import pandas as pd 
-
-wsb = pd.read_csv('/Users/jeremiahkamama/Desktop/Article Projects/Spacy-Sent/data/reddit_wsb.csv')
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp(wsb)
-
